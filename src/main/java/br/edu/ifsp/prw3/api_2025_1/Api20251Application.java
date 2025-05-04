998765dcb4d5d222ec8a9e296bd82af672362889
Pedro Bonelli <pebonellimecca@gmail.com>
2025-05-04 19:26:59 -0300

    adicionando dependencias e pasta target

--- src/main/java/br/edu/ifsp/prw3/api_2025_1/Api20251Application.java
+++ src/main/java/br/edu/ifsp/prw3/api_2025_1/Api20251Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Api20251Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Api20251Application.class, args);
 	}
 
