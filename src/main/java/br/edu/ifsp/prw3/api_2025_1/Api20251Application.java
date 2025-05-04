1f3ba423b4ac9c6c4a2b01e3a48cd16380e7b367
Pedro Bonelli <pebonellimecca@gmail.com>
2025-05-04 18:54:24 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifsp/prw3/api_2025_1/Api20251Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prw3.api_2025_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api20251Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api20251Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/prw3/api_2025_1/Api20251ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.prw3.api_2025_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api20251ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
